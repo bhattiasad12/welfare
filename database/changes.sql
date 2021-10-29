eb15779896abc2da043fed30b349a5cdd9545bc4
tabraiz.anwer@koderlabs.com <tabraiz.anwer@koderlabs.com>
2021-10-29 16:32:22 +0500

    db changes

--- database/changes.sql
+++ database/changes.sql
@@ -9,3 +9,9 @@ CREATE TABLE `user_rights` (
 
 
 =======================================================
+
+
+CREATE TABLE `welfare`.`application_documents`(     `document_id` BIGINT(10) ,     `application_id` BIGINT(1) ,     `document_name` VARCHAR(255) ,     `document_path` VARCHAR(5000) ,     `created_at` DATETIME   );
+
+
+ALTER TABLE `welfare`.`application_documents`     CHANGE `document_id` `document_id` BIGINT(10) NOT NULL AUTO_INCREMENT,    ADD PRIMARY KEY(`document_id`);
